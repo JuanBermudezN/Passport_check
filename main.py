4aadf7b53a193723f266f5bcf0fb133fff7d9115
Juan Bermudez <juanbermudeznunez@gmail.com>
2022-11-12 19:33:44 -0300

    Add validation to code

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ params_req_embajada ={"cedula": CEDULA_IDENTIDAD, "buscar": "Consultar"}
 
 def lambda_handler(event, context):
     response_req_embajada = requests.post(URL_EMBAJADA,data=params_req_embajada, headers=headers_req_embajada)
+    if(response_req_embajada.status_code) >= 400:
+        return {"message" : "ERROR: The request to the embassy has failed"}
     for c in response_req_embajada.iter_lines():
         primera_linea_response = str(c)
         break
     mensaje_embajada = primera_linea_response[primera_linea_response.find("(")+1:primera_linea_response.find(")")]
     WSP_API_REQ = f"{WSP_API_URL}?phone={PHONE_NUMBER}&text={mensaje_embajada}&apikey={WSP_API_KEY}"
     wsp_api_response = requests.get(WSP_API_REQ)
-    return {"message" : "OK" if (wsp_api_response.status_code) in (200, 201) else "ERROR" }
+    return {"message" : "OK" if (wsp_api_response.status_code) in (200, 201) else "ERROR: The callmebot API has failed" }
